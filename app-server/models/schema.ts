11bbd97910e7cd397da34070b0d017785e4fb5dc
Abbatyya <abbaty48@gmail.com>
2023-03-01 13:03:21 +0100

    -- IMPLEMENT APP-SERVER -- Implement Warehouse - schema,interface,model,models,resolvers

--- app-server/models/schema.ts
+++ app-server/models/schema.ts
@@ -2,15 +2,20 @@ import { gql } from 'apollo-server-express';
 
 const typeDef = gql(`
    ####### INPUTS #######
- 
    input categoryInput {
       name: String
    }
+
    ####### PAYLOADS ######
    type categoryPayload {
       error: String
    }
    ####### TYPES ########
+
+   ## CATEGORY
+   type Category {
+      name: String!
+   }
    type AddCategoryPayload {
       error: String
       added: Boolean!
@@ -25,18 +30,46 @@ const typeDef = gql(`
       error: String
       deleted: Boolean!
    }
-   type Category {
-      name: String!
+   ## WAREHOUSE
+   type Warehouse {
+      warehouseID: ID!
+      name: String
+      address: String!
+      staffs: [Staff!]!
+      products: [Product!]!
    }
+   ### WAREHOUSE PAYLOADS
+   type AddWarehousePayload {
+      error: String
+      added: Boolean!
+      newAdded: Warehouse
+   }
+   type EditWarehousePayload {
+      error: String
+      edited: Boolean!
+   }
+   type DeleteWarehousePayload {
+      error: String
+      deleted: Boolean!
+   }
+   type AddWarehouseStaffPayload {
+      error: String
+      added: Boolean!
+   }
+   ## 
    type Query {
       categories: [Category!]!
+      warehouses: [Warehouse!]!
    }
    type Mutation {
       addCategory(category: String!): AddCategoryPayload
       editCategory(oldCategory: String!, newCategory: String!): EditCategoryPayload
       deleteCategory(category: String!): DeleteCategoryPayload
+
+      addWarehouse(warehouseID: ID!, name: String, address: String!, staffs: [Staff], products: [Product]): AddWarehousePayload
+      editWarehouse(warehouseID: ID!, name: String, address: String, staffs: [Staff!], products: [Product!]): EditWarehousePayload
+      deleteWarehouse(warehouseID: ID!): DeleteWarehousePayload
    }
-   #type Subscription {}
 `);
 
 export default typeDef;
--- app-server/resolvers/index.resolvers.ts
+++ app-server/resolvers/index.resolvers.ts
@@ -1,13 +1,24 @@
-import { ICategory } from '@server-databases/mongodb/interfaces/ICategory';
 import { CategoryResolver } from '@server-resolvers/category.resolver';
+import { WarehouseResolver } from '@server-resolvers/warehouse.resolver';
+import { ICategory } from '@server-databases/mongodb/interfaces/ICategory';
+import { IWarehouse } from '@server-databases/mongodb/interfaces/IWarehouse';
+import { IResolverContext } from '@server-commons/models/interfaces/IResolverContext';
 
 export const resolvers = {
   Query: {
     categories: async (_: any): Promise<ICategory[]> => {
       return await CategoryResolver.categories();
     },
+    warehouses: async (
+      _: any,
+      args: any,
+      context: IResolverContext
+    ): Promise<IWarehouse[]> => {
+      return await WarehouseResolver.warehouses(context);
+    },
   },
   Mutation: {
+    // CATEGORY
     addCategory: async (_: any, { category }: any) => {
       return await CategoryResolver.addCategory(category);
     },
@@ -16,6 +27,44 @@ export const resolvers = {
     },
     deleteCategory: async (_: any, { category }: any) => {
       return await CategoryResolver.deleteCategory(category);
+    },
+    // WAREHOUSE
+    addWarehouse: async (
+      _: any,
+      { warehouseID, address, staffs, products }: any,
+      { request, response, config }: IResolverContext
+    ) => {
+      return await WarehouseResolver.addWarehouse(
+        {
+          warehouseID,
+          address,
+          staffs,
+          products,
+        },
+        { request, response, config }
+      );
+    },
+    editWarehouse: async (
+      _: any,
+      { warehouseID, address, staffs, products }: any,
+      { request, response, config }: IResolverContext
+    ) => {
+      return await WarehouseResolver.editWarehouse(warehouseID, {
+        request,
+        response,
+        config,
+      });
+    },
+    deleteWarehouse: async (
+      _: any,
+      { warehouseID }: any,
+      { request, response, config }: IResolverContext
+    ) => {
+      return await WarehouseResolver.deleteWarehouse(warehouseID, {
+        request,
+        response,
+        config,
+      });
     },
   },
 };
